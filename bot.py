b670faf33bdf513fd26eea9e689abf1e3feb5918
cuxhausen <cuxhausen@mail.ru>
2021-11-28 19:39:30 +0600

    changed the code

--- bot.py
+++ bot.py
@@ -1,46 +1,53 @@
 import telebot
+from telebot import types
 
 token = '2138237247:AAGOfmz-0-_luhmENRbxDDx5v6HFaLnfIrU'
 
-bot = telebot.TeleBot(token) # Объявляем бота
+pizza_size = ''
+payment = ''
+#age = 0
 
+bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['start'])
-def start_message(message):
-    keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('Привет', 'Пока')
-    bot.send_message(message.chat.id, 'Привет!', reply_markup=keyboard)
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Вас приветствует бот-ассистент. Я помогу Вам выбрать и заказать пиццу! Чтобы начать делать заказ введи команду /order")
 
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    if message.text == 'Привет':
+        bot.reply_to(message, 'Привет дорогой любитель пиццы!')
+    elif message.text == 'hi':
+        bot.reply_to(message, 'Hello dear pizza lover!')
+    elif message.text == '/order':
+        bot.send_message(message.from_user.id, "Какую Вы хотите пиццу? Большую или маленькую?")
+        bot.register_next_step_handler(message, reg_pizza_size)
 
-@bot.message_handler(commands=['test'])
-def start_message(message):
-    markup = telebot.types.InlineKeyboardMarkup()
-    markup.add(telebot.types.InlineKeyboardButton(text='Три', callback_data=3))
-    markup.add(telebot.types.InlineKeyboardButton(text='Четыре', callback_data=4))
-    markup.add(telebot.types.InlineKeyboardButton(text='Пять', callback_data=5))
-    bot.send_message(message.chat.id, text="Какая средняя оценка была у Вас в школе?", reply_markup=markup)
+def reg_pizza_size(message):
+    global pizza_size
+    pizza_size = message.text
+    bot.send_message(message.from_user.id, "Как Вы будите платить? Наличкой или картой?")
+    bot.register_next_step_handler(message, reg_payment)
 
+def reg_payment(message):
+    global payment
+    payment = message.text
 
+    keyboard = types.InlineKeyboardMarkup()
+    key_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+    keyboard.add(key_yes)
+    key_no = types.InlineKeyboardButton(text='Нет', callback_data='no')
+    keyboard.add(key_no)
+    question = 'Вы хотите ' + pizza_size + ' пиццу, оплата - ' + payment + '?'
+    bot.send_message(message.from_user.id, text = question, reply_markup=keyboard)
+    
 @bot.callback_query_handler(func=lambda call: True)
-def query_handler(call):
-    bot.answer_callback_query(callback_query_id=call.id, text='Спасибо за честный ответ!')
-    answer = ''
-    if call.data == '3':
-        answer = 'Вы троечник!'
-    elif call.data == '4':
-        answer = 'Вы хорошист!'
-    elif call.data == '5':
-        answer = 'Вы отличник!'
-    bot.send_message(call.message.chat.id, answer)
-    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+def callback_worker(call):
+    if call.data == "yes":
+        bot.send_message(call.message.chat.id, "Заказ принят. Ожидайте подтверждения.")
+    elif call.data == "no":
+        bot.send_message(call.message.chat.id, "Давайте попробуем еще раз!")
+        bot.send_message(call.message.chat.id, "Какую Вы хотите пиццу? Большую или маленькую?")
+        bot.register_next_step_handler(call.message, reg_pizza_size)
 
-
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, 'Ещё раз привет!')
-    elif message.text.lower() == 'пока':
-        bot.send_message(message.chat.id, 'Пока!')
-
-
-bot.polling(none_stop=True, interval=0)
+bot.polling()
